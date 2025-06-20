f7972f4a8fd519d534a09ceb3ed51a4069a63caf
KSayanta <sayantakarmakar1999@gmail.com>
2025-06-20 13:03:22 +0530

    add fetch function

---
+++ src/api.ts
@@ -0,0 +1,112 @@
+import { z } from "zod/v4";
+const url = "https://opentdb.com/api.php?amount=5&type=multiple&encode=base64";
+
+const _questionSchema = z.object({
+  type: z.base64(),
+  difficulty: z.base64(),
+  category: z.base64(),
+  question: z.base64(),
+  correct_answer: z.base64(),
+  incorrect_answers: z.array(z.base64()),
+});
+
+export type Question = z.infer<typeof _questionSchema>;
+
+type ApiResponse = {
+  response_code: number;
+  results: Question[];
+};
+
+export default async function fetchQuestions(): Promise<Question[]> {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`NetWork error! ${response.status}`);
+    }
+
+    const data: ApiResponse = await response.json();
+    if (data.response_code) {
+      throw new Error(
+        `API error! Check documentation. Error Code=${data.response_code}`
+      );
+    }
+
+    const result = _questionSchema.array().safeParse(data.results);
+    if (!result.success) {
+      throw new Error(`Invalid API response ${z.prettifyError(result.error)}`);
+    }
+
+    return result.data;
+  } catch (err) {
+    const msg =
+      err instanceof Error ? err.message : `There was an error! ${err}`;
+    console.error(msg);
+    return [];
+  }
+}
+
+/**
+ * Demo response
+  const data = {
+    response_code: 0,
+    results: [
+      {
+        type: "bXVsdGlwbGU=",
+        difficulty: "aGFyZA==",
+        category: "SGlzdG9yeQ==",
+        question:
+          "SW4gd2hpY2ggeWVhciBkaWQgdGhlIFRva3lvIFN1YndheSBTYXJpbiBBdHRhY2sgb2NjdXI\/",
+        correct_answer: "MTk5NQ==",
+        incorrect_answers: ["MjAwMQ==", "MjAxMQ==", "MTk5MQ=="],
+      },
+      {
+        type: "bXVsdGlwbGU=",
+        difficulty: "bWVkaXVt",
+        category: "RW50ZXJ0YWlubWVudDogTXVzaWM=",
+        question:
+          "SW4gd2hhdCB5ZWFyIGRpZCBVUyBiYW5kIFdlZXplciByZWxlYXNlIFdlZXplciAoVGhlIFdoaXRlIEFsYnVtKT8=",
+        correct_answer: "MjAxNg==",
+        incorrect_answers: ["MjAwMQ==", "MjAwOA==", "MjAxOQ=="],
+      },
+      {
+        type: "bXVsdGlwbGU=",
+        difficulty: "bWVkaXVt",
+        category: "SGlzdG9yeQ==",
+        question:
+          "VGhlIFBhbmFtYSBDYW5hbCB3YXMgZmluaXNoZWQgdW5kZXIgdGhlIGFkbWluaXN0cmF0aW9uIG9mIHdoaWNoIFUuUy4gcHJlc2lkZW50Pw==",
+        correct_answer: "V29vZHJvdyBXaWxzb24=",
+        incorrect_answers: [
+          "RnJhbmtsaW4gRGVsYW5vIFJvb3NldmVsdA==",
+          "SGVyYmVydCBIb292ZXI=",
+          "VGhlb2RvcmUgUm9vc2V2ZWx0",
+        ],
+      },
+      {
+        type: "bXVsdGlwbGU=",
+        difficulty: "aGFyZA==",
+        category: "RW50ZXJ0YWlubWVudDogVmlkZW8gR2FtZXM=",
+        question:
+          "SW4gIlRoZSBCaW5kaW5nIG9mIElzYWFjIiwgd2hpY2ggaXRlbSBpbnN0YW50bHkga2lsbHMgTW9tIGFuZCBNb20ncyBIZWFydD8=",
+        correct_answer: "VGhlIEJpYmxl",
+        incorrect_answers: [
+          "VGhlIEhhbG8=",
+          "QnJpbXN0b25l",
+          "Qm9vayBvZiBTaGFkb3dz",
+        ],
+      },
+      {
+        type: "bXVsdGlwbGU=",
+        difficulty: "bWVkaXVt",
+        category: "RW50ZXJ0YWlubWVudDogTXVzaWM=",
+        question:
+          "V2hpY2ggc29uZyBtYWRlIGJ5IE1BTiBXSVRIIEEgTUlTU0lPTiB3YXMgdXNlZCBhcyB0aGUgb3BlbmluZyBmb3IgdGhlIGFuaW1lICJMb2cgSG9yaXpvbiI\/",
+        correct_answer: "IkRhdGFiYXNlIg==",
+        incorrect_answers: [
+          "IkRlYWQgRW5kIGluIFRva3lvIg==",
+          "IlJhaXNlIFlvdXIgRmxhZyI=",
+          "Ik91dCBvZiBDb250cm9sIg==",
+        ],
+      },
+    ],
+  };
+ */
