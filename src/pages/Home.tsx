2b52921d6e63b7ae9672652bd63038030649d31b
KSayanta <sayantakarmakar1999@gmail.com>
2025-06-21 10:45:34 +0530

    chore update fonts, remove boilerplate code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,8 +6,11 @@ export default function Home() {
     <section className="home">
       <h1 className="title-1 home__heading">Quizzical</h1>
       <p className="text-2 home__description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
-        quisquam.
+        The largest quiz, according to Guinness, was the "Quiz for Life", held
+        at the Flanders Expo Halls in Ghent, Belgium, on 11 December 2010 with
+        2,280 participants. The winning team Café De Kastaar from Leuven
+        consisted of Marnix Baes, Erik Derycke, Eric Hemelaers, Bart Permentier
+        and Tom Trogh.
       </p>
       <Link to="quiz" className="btn btn--cta btn--lg">
         Start Quiz
