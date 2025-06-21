623e65014a486d6986f246b621d67b5a9e2f8e7d
KSayanta <sayantakarmakar1999@gmail.com>
2025-06-21 18:05:45 +0530

    refactor routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,27 +1,40 @@
-import { createBrowserRouter } from "react-router";
+import {
+  createBrowserRouter,
+  type DOMRouterOpts,
+  type RouteObject,
+} from "react-router";
 import Home, { loader as homeLoader } from "./pages/Home";
 import Quiz from "./pages/Quiz";
 import ErrorElement from "./pages/ErrorElement";
 import PageNotFound from "./pages/PageNotFound";
 
-export const router = createBrowserRouter([
+const opts: DOMRouterOpts = {
+  basename: "/quizzical",
+};
+
+const routes: RouteObject[] = [
   {
-    path: "/quizzical",
+    path: "/",
     errorElement: <ErrorElement />,
+
     children: [
       {
         index: true,
-        Component: Home,
+        element: <Home />,
         loader: homeLoader,
       },
+
       {
         path: "quiz",
-        Component: Quiz,
-      },
-      {
-        path: "*",
-        Component: PageNotFound,
+        element: <Quiz />,
       },
     ],
   },
-]);
+
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+];
+
+export const router = createBrowserRouter(routes, opts);
